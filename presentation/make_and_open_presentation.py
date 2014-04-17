3b4d7db67a2129f09eb0560862c0ca0a0c895f86
Edward Kemper <edwardkemper@gmail.com>
2014-04-17 09:02:35 -0700

    progress 4-17-14

--- presentation/make_and_open_presentation.py
+++ presentation/make_and_open_presentation.py
@@ -10,14 +10,14 @@ elif len(sys.argv) == 1:
 
     page_number = '1'
 
-system('pdflatex presentation_1.tex')
+system('pdflatex ekemper_defense_presentation.tex')
 
 #system('bibtex main.aux')
 
-system('pdflatex presentation_1.tex')
+system('pdflatex ekemper_defense_presentation.tex')
 
 #system('pkill -3 evin')
 
-system('evince --page-label='+ page_number +' presentation_1.pdf &')
+system('evince --page-label='+ page_number +' ekemper_defense_presentation.pdf &')
 
 #system('gnome-open main.pdf &')
